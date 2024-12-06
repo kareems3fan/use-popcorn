c932ef19b1540d38aca9e5b266d72f08995c4362
kareems3fan <martogitso@gmail.com>
2024-12-06 22:19:09 +0200

    Delete unused file and update useMovies.js

--- src/useMovies.js
+++ src/useMovies.js
@@ -9,46 +9,48 @@ export function useMovies(query, callback) {
     function () {
       callback?.();
       const controller = new AbortController();
-      async function fetchMovies()
-      {try{
-        setIsLoading(true);
-        setError("")
-        const res = await fetch( `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-          { signal: controller.signal })
-          if(!res.ok)
-          {
-            throw new error("there is something wrong in fetching data")
-          }
-          const data = await res.json();
-          if(data.Response==="False") throw new error("error in fetching this movie");
-          setMovies(data.search);
+      async function fetchMovies() {
+        try {
+          setIsLoading(true);
           setError("");
-         
-          
-          
 
-      } catch(err)
-      {
-        if(err.name!=="AbortError")
-        {
-          console.log(err.message);
-          setError("err.message")
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
+          );
+
+          if (!res.ok)
+            throw new Error("Something went wrong with fetching movies");
+
+          const data = await res.json();
+          if (data.Response === "False") throw new Error("Movie not found");
+
+          setMovies(data.Search);
+          setError("");
+        } catch (err) {
+          if (err.name !== "AbortError") {
+            console.log(err.message);
+            setError(err.message);
+          }
+        } finally {
+          setIsLoading(false);
         }
-       
-      } finally {
-        setIsLoading(false);
       }
-    }
-    if (query.length < 3) {
-      setMovies([]);
-      setError("");
-      return;
-    }
-    fetchMovies();
-    return function ()
-    {
-      controller.abort();
-    }
-      },[query])
+
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+
+      fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
+    },
+    [query]
+  );
+
   return { movies, isLoading, error };
 }
